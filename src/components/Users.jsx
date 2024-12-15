715de2a0c07ade3825222c4a99c078cbdbdcffa3
Ashik <abdullahashik17@gmail.com>
2024-12-16 01:44:13 +0600

    client site

---
+++ src/components/Users.jsx
@@ -0,0 +1,29 @@
+import { useLoaderData } from "react-router-dom";
+
+const Users = () => {
+    const users = useLoaderData()
+
+    const handleDelete = _id=>{
+        console.log(_id);
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method: 'DELETE'
+        })
+        .then(res=>res.json())
+        .then(data=> {
+            console.log(data);
+            if(data.deletedCount > 0){
+                alert('deleted successfully')
+            }
+        })
+    }
+    return (
+        <div>
+            <p>{users.length}</p>
+            {
+                users.map(user=> <p key={user._id}>{user.name} : {user.email} <button onClick={()=> handleDelete(user._id)}>X</button></p> )
+            }
+        </div>
+    );
+};
+
+export default Users;
