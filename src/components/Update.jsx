5d6dd1a184a0009f4114967d59fccfbe9c7ecbd8
Ashik <abdullahashik17@gmail.com>
2024-12-17 01:39:53 +0600

    basic complete

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -3,11 +3,26 @@ import { useLoaderData } from "react-router-dom";
 
 const Update = () => {
     const loadedUsers = useLoaderData()
+    console.log(loadedUsers);
     const handleSubmit = event=>{
         event.preventDefault()
         const form = event.target;
         const name = form.name.value;
         const email = form.email.value;
+        const user = {name,email}
+        console.log(name,email);
+
+        fetch(`http://localhost:5000/users/${loadedUsers._id}`,{
+            method: 'PUT',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res=> res.json())
+        .then(data=> {
+            console.log(data);
+        })
     }
 
     return (
